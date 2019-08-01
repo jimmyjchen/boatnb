0c58e52769d2812c3d4137b04c615866e3349345
Bingqing Zhu <bingbing13@163.com>
2019-07-31 21:37:51 -0700

    added basic map on index page

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,6 @@ class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :description, :price, :title, :photo, presence: true, allow_blank: false
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
